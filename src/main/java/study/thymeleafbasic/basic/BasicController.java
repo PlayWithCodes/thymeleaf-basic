01b42edec186fc276dc785dce1d8c770c2afd3f4
turtle <michael.app.space@gmail.com>
2022-05-29 15:29:27 +0900

    study: try springEL

--- src/main/java/study/thymeleafbasic/basic/BasicController.java
+++ src/main/java/study/thymeleafbasic/basic/BasicController.java
@@ -1,9 +1,16 @@
 package study.thymeleafbasic.basic;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/basic")
@@ -14,6 +21,35 @@ public class BasicController {
         model.addAttribute("data", "Hello thymeleaf!");
         model.addAttribute("escape", "<b>escape</b>");
         model.addAttribute("unescape", "<b>unescape</b>");
+
         return "/basic/text-basic";
     }
+
+    @GetMapping("/variable")
+    public String variable(Model model) {
+        User userA = new User("userA", 10);
+        User userB = new User("userB", 20);
+
+        List<User> userList = new ArrayList<>();
+        userList.add(userA);
+        userList.add(userB);
+
+        Map<String, User> userMap = new HashMap<>();
+        userMap.put("userA", userA);
+        userMap.put("userB", userB);
+
+        model.addAttribute("user", userA);
+        model.addAttribute("userList", userList);
+        model.addAttribute("userMap", userMap);
+
+        return "/basic/variable";
+    }
+
+    @AllArgsConstructor
+    @Getter
+    static class User {
+        private String username;
+        private int age;
+    }
+
 }
