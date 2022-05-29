ac3513160d6ae08fb113a4962080ae89f71c523b
turtle <michael.app.space@gmail.com>
2022-05-29 16:29:30 +0900

    study: try expression-utility-objects

--- src/main/java/study/thymeleafbasic/basic/BasicController.java
+++ src/main/java/study/thymeleafbasic/basic/BasicController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpSession;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -67,4 +68,9 @@ public class BasicController {
         }
     }
 
+    @GetMapping("/date")
+    public String date(Model model) {
+        model.addAttribute("localDateTime", LocalDateTime.now());
+        return "/basic/date";
+    }
 }
