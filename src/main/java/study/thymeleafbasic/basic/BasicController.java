ebddac32ea540a3d8b803f8c42ffda3d86392d6e
turtle <michael.app.space@gmail.com>
2022-05-29 20:22:44 +0900

    study: try thymeleaf block

--- src/main/java/study/thymeleafbasic/basic/BasicController.java
+++ src/main/java/study/thymeleafbasic/basic/BasicController.java
@@ -125,4 +125,10 @@ public class BasicController {
         model.addAttribute("data", "Thymeleaf!");
         return "/basic/comments";
     }
+
+    @GetMapping("/block")
+    public String block(Model model) {
+        addUsers(model);
+        return "/basic/block";
+    }
 }
