052ca3a5b9dad3afe6746ed9004aa4f1af005a15
turtle <michael.app.space@gmail.com>
2022-05-29 22:09:46 +0900

    study: try thymeleaf template layout 2

--- src/main/java/study/thymeleafbasic/basic/TemplateController.java
+++ src/main/java/study/thymeleafbasic/basic/TemplateController.java
@@ -1,7 +1,6 @@
 package study.thymeleafbasic.basic;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -18,5 +17,10 @@ public class TemplateController {
     @GetMapping("/layout")
     public String layout() {
         return "/template/layout/layoutMain";
+    }
+
+    @GetMapping("/layoutExtend")
+    public String layoutExtend() {
+        return "template/layoutExtend/layoutExtendMain";
     }
 }
